8ded094add04e201727ebfbd02b46b11a730d6ed
Olexander Hofman <oh@mamoru.network>
2023-10-23 11:39:52 +0200

    fix tracer, clear tracer on worker config

--- miner/worker.go
+++ miner/worker.go
@@ -19,6 +19,7 @@ package miner
 import (
 	"errors"
 	"fmt"
+	"github.com/ethereum/go-ethereum/core/vm"
 	"math/big"
 	"sync"
 	"sync/atomic"
@@ -865,7 +866,16 @@ func (w *worker) commitTransaction(env *environment, tx *types.Transaction) ([]*
 		snap = env.state.Snapshot()
 		gp   = env.gasPool.Gas()
 	)
-	receipt, err := core.ApplyTransaction(w.chainConfig, w.chain, &env.coinbase, env.gasPool, env.state, env.header, tx, &env.header.GasUsed, *w.chain.GetVMConfig())
+	//////////////////// MAMORU ////////////////////
+	// Clear Mamoru tracer
+	vmConfig := vm.Config{
+		Tracer:                  nil,
+		NoBaseFee:               w.chain.GetVMConfig().NoBaseFee,
+		EnablePreimageRecording: w.chain.GetVMConfig().EnablePreimageRecording,
+		ExtraEips:               w.chain.GetVMConfig().ExtraEips,
+	}
+	//////////////////// MAMORU ////////////////////
+	receipt, err := core.ApplyTransaction(w.chainConfig, w.chain, &env.coinbase, env.gasPool, env.state, env.header, tx, &env.header.GasUsed, vmConfig)
 	if err != nil {
 		env.state.RevertToSnapshot(snap)
 		env.gasPool.SetGas(gp)
